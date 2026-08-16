2abed724f5ab6d264dcef9929c0a524001b54bd9
Sponsornet <sponsornet12345@gmail.com>
2026-08-16 22:41:03 +0300

    Update molestrology.py

--- molestrology.py
+++ molestrology.py
@@ -145,7 +145,7 @@ async def photo_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         prompt = prompt_en if lang == "en" else prompt_ua
 
-        url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={GEMINI_API_KEY}"
+        url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key={GEMINI_API_KEY}"
 
         payload = {
             "contents": [{
